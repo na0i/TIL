fc4b0f11448e551c0f3ccffe108a746fa259c0bf
na0i <nayoi1oo5@gmail.com>
2021-06-14 21:52:56 +0900

    0614 TIL

--- study_algorithm/0610/SWEA_4875_미로.py
+++ study_algorithm/0610/SWEA_4875_미로.py
@@ -10,13 +10,39 @@ for tc in range(T):
     for _ in range(N):
         miro.append(list(map(int, input())))
 
-    print(miro)
+    # print(miro)  # 미로 길
 
     for i in range(N):
         for j in range(N):
             if miro[i][j] == 2:
-                start_x, start_y = i, j
+                start_r, start_c = i, j
             elif miro[i][j] == 3:
-                end_x, end_y = i, j
+                end_r, end_c = i, j
 
-    print(start_x, start_y, end_x, end_y)
+    # print(start_r, start_c, end_r, end_c)  # 시작점 좌표, 도착점 좌표
+
+
+    # 상하좌우 delta
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+    # stack 초깃값
+    stack = [(start_r, start_c)]
+    while stack:
+        current = stack.pop()
+
+        for d in range(4):
+            next_r = current[0] + dr[d]
+            next_c = current[1] + dc[d]
+
+            if 0 <= next_r < N and 0 <= next_c < N and miro[next_r][next_c] == 0 and visit[next_r][next_c] == 0:
+                stack.append((next_r, next_c))
+
+            elif next_r == end_r and next_c == end_c:
+                visit[end_r][end_c] = 1
+                print('#{} {}'.format(tc+1, '1'))
+                break
+        visit[current[0]][current[1]] = 1
+
+    if visit[end_r][end_c] == 0:
+        print('#{} {}'.format(tc+1, '0'))
+
